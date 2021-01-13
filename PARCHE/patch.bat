262eb856c9e79aaf4e5f7c623616e49384d81bf4
AntonioClares <toni12883@gmail.com>
2021-01-13 16:59:29 +0100

    Update patch.bat

--- PARCHE/patch.bat
+++ PARCHE/patch.bat
@@ -1,5 +1,6 @@
 MKDIR "C:/Windows/system32/Macromed/Flash"
 MKDIR "C:/Windows/SysWOW64/Macromed/Flash"
-copy /Y %~dp0mms.cfg "C:/Windows/system32/Macromed/Flash"
-copy /Y %~dp0mms.cfg "C:/Windows/SysWOW64/Macromed/Flash"
+copy /Y %~dp0mms.cfg %SYSTEMDRIVE%"/Windows/system32/Macromed/Flash"
+copy /Y %~dp0mms.cfg %SYSTEMDRIVE%"/Windows/SysWOW64/Macromed/Flash"
+ECHO %SYSTEMDRIVE%
 pause
