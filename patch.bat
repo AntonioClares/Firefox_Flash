92974853f0c17d40ae86f3fa17175ed771e06e02
AntonioClares <toni12883@gmail.com>
2021-01-13 15:18:16 +0100

    Delete patch.bat

--- patch.bat
+++
@@ -1,5 +0,0 @@
-MKDIR "C:/Windows/system32/Macromed/Flash"
-MKDIR "C:/Windows/SysWOW64/Macromed/Flash"
-copy /Y %~dp0mms.cfg "C:/Windows/system32/Macromed/Flash"
-copy /Y %~dp0mms.cfg "C:/Windows/SysWOW64/Macromed/Flash"
-pause
